9e77e191b1ea6e1625f3b044fe5646976b70395b
mkrum <mrkrumdick@gmail.com>
2015-10-28 01:02:21 -0400

    testing

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var twilio = require('twilio')('ACdc7d3faac00d72c93a830191947c999a', 'dccfe5571db0d393c727cee38b68a730');
 var express = require('express');
-var bodyParser = require('body-parser'); 
+var compression = require('compression');
 var app = express();
 
-app.use(express.urlencoded());
 
 app.post('/recieveSMS', function(req, res) {
 	console.log(req.body.Body);
