f084d9c773183758dc68aaa4b117cb278651f77a
mkrum <mrkrumdick@gmail.com>
2015-11-01 11:06:26 -0500

    test

--- app.js
+++ app.js
@@ -4,25 +4,13 @@ var bodyParser = require('body-parser');
 var queryString = require('querystring');
 var app = express();
 
-var clientID = '0095976fe9c24fc5a6e4a7559e01f37e';
-var clientSecret = '967795bf432646f69797a1a7e7d97a0e';
-var redirect_uri = 'http://jamocracy.herokuapp.com/auth';
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extend: true}));
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/login', function(req, res) {
-	var scope = 'playlist-read-private playlist-read-colloborative playlist-modify-public playlist-modify-private user-read-private';
-	res.redirect('https://accounts.spotify.com/authorize?' +
-			queryString.stringify({
-				response_type: 'code',
-				client_id: client_id,
-			    scope: scope,
-			    redirect_uri: redirect_uri
-    }));
+app.get('/auth', function(req, res) {
+	res.send("test");
 });
 
 
@@ -39,10 +27,7 @@ app.post('/SMS', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-		var btn = document.createElement('BUTTON');
-		var text = document.createTextNode("Log in");
-		btn.appendChild(text);
-		document.body.appendChild(btn);
+		
 });
 
 var port = process.env.PORT || 8080;
