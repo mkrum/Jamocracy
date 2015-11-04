06565dfe2a52ca44d13b5006d8e176ab73ff8567
mkrum <mrkrumdick@gmail.com>
2015-11-03 22:41:58 -0500

    test

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/auth', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname+'/index.html'));
+	res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 
