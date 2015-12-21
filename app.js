38f90d4a83764b5b86a266fcd9425a6b361e3dea
mkrum <mrkrumdick@gmail.com>
2015-12-21 12:39:50 -0600

    Working on text reception

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.post('/SMS', function(req, res) {
 				});
 			} else {
 				var songObj = JSON.parse(body);
-				console.log(songObj.tracks.items[3]);
+				console.log(songObj.tracks.items.name);
 				twilio.messages.create({ 
 					to: '16304325433', 
 					from: "+16305818347", 
