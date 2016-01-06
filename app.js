2e0e6421cda84db63f177abab65c3893221e1149
mkrum <mrkrumdick@gmail.com>
2016-01-05 20:06:18 -0600

    added a way to exit

--- app.js
+++ app.js
@@ -143,8 +143,8 @@ app.post('/success', function(req, res) {
 app.post('/SMS', function(req, res){
     var playlist, partyCode;
     // check if sender is in numbers collection
-    db.get('numbers', req.body.From.substring(2)) // ignore the '+1' prefix
 	sendText("Here", req.body.From);
+    db.get('numbers', req.body.From.substring(2)) // ignore the '+1' prefix
     .then(function(res){ // if it is found in numbers
         console.log("found");
 		if(req.body.Body[0] === '!'){
