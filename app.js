2da832f15b1a521151e9717033ee2c262790faec
Daniel Kerrigan <dkerriga@nd.edu>
2016-01-26 21:20:14 -0500

    debugging

--- app.js
+++ app.js
@@ -194,7 +194,7 @@ app.post('/SMS', function(req, res){
 					db.get('parties', partyCode) // search the parties collection for this code
 					.then(function(data){
 						playlist = data.body; // get the playlist for this party
-						removeSong(song, playlist, partyCode);
+						//removeSong(song, playlist, partyCode);
 					})
 					.fail(function(err){
 						console.log('error conecting to playlist');
@@ -205,7 +205,7 @@ app.post('/SMS', function(req, res){
             partyCode = numRes.body.party;
             db.get('parties', partyCode) // search the parties collection for this code
             .then(function(data){
-				updateSong('null', number);
+				//updateSong('null', number);
                 playlist = data.body; // get the playlist for this party
                 getSong(req.body, playlist, partyCode);
             })
