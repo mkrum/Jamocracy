b22541b96724db0587ab2f026837471be821f951
mkrum <mrkrumdick@gmail.com>
2016-02-08 19:52:06 -0500

    Added an analytics folder

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -1,5 +1,13 @@
 from porc import Client
+from collections import defaultdict
+import csv
 client = Client('f61515c7-8df9-4003-ab45-2f3e259610ff')
+
+reader = csv.reader(open('areacodes.csv', 'r'))
+d = {}
+for row in reader:
+   k, v = row
+   d[k] = v
 
 def displayNumUsers():
     numbers = client.list('numbers').all()
@@ -14,9 +22,9 @@ def mostPopularSong():
             max = songs[i]['value']['playCount']
             songName = songs[i]['path']['key']
             song = songs[i]
-
     print "Most Popular Song: ", songName
     print "Plays: ", max
+    getAreaCodes(song)
 
 def displayNumSongs():
     songs = client.list('songs').all()
@@ -32,18 +40,21 @@ def printNumbers():
     for i in range(0, len(numbers)):
         print numbers[i]
 
+#song object from the database
 def getAreaCodes(song):
-    # song = client.get('songs', song)
-
     numbers = song['value']['numbers']
+    Numb = defaultdict(int)
     for i in range(0, len(numbers)):
-        print numbers[i][2:5]
+        Numb[numbers[i][2:5]] += 1
+    codes = sorted(Numb)
+    for i in range(0, len(codes)):
+        print d[codes[len(codes) - i - 1]],"     ", Numb[codes[len(codes) - i - 1]]
 
 if __name__ == '__main__':
     print " JAMOCRACY STATS"
     displayNumUsers()
     mostPopularSong()
     displayNumSongs()
-    #printSongs()
-
+    printSongs()
+    
      
