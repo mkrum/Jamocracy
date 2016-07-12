e211c04abb1bdf867dd5e684bc816d916f993459
Sam Calvert <sam.clvrt@gmail.com>
2016-07-11 21:05:59 -0700

    Move import to top

--- routes/login_route.js
+++ routes/login_route.js
@@ -1,6 +1,6 @@
-exports.setup = (app) => {
-    const SpotifyService = require('../services/spotify_api_service');
+const SpotifyService = require('../services/spotify_api_service');
 
+exports.setup = (app) => {
     app.get('/login', (req, res) => {
         res.redirect(SpotifyService.authorizeURL);
     });
