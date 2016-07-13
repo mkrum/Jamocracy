4513b916a9586e7b00bbdbb2bb4cb19df4167365
Sam Calvert <sam.clvrt@gmail.com>
2016-07-12 22:28:56 -0700

    Take advantage of beforeEach

--- spec/routes/submit_route_spec.js
+++ spec/routes/submit_route_spec.js
@@ -15,83 +15,16 @@ describe('POST /submit', () => {
         mockery.disable();
     });
 
-    afterEach(() => {
-        mockery.resetCache();
-    });
+    var app,
+        access_token,
+        refresh_token,
+        route,
+        phoneNumber,
+        username,
+        playlistId,
+        isNewPlaylist;
 
-    it('handles an existing playlist', (done) => {
-        var access_token,
-            refresh_token,
-            route,
-            phoneNumber,
-            username,
-            playlistId,
-            isNewPlaylist;
-        const spotifyMock = {
-            setTokens: (access, refresh) => {
-                access_token = access;
-                refresh_token = refresh;
-            },
-
-            getCurrentUser: () => Promise.resolve(1234)
-        };
-
-        const requestMock = {
-            post: (postRoute, params) => {
-                route = postRoute;
-                phoneNumber = params.form.number;
-                playlistId = params.form.playlist;
-                username = params.form.name;
-                isNewPlaylist = params.form.isNewPlaylist;
-            }
-        };
-
-        const hostMock = {
-            makeUri: () => "generated uri"
-        };
-
-        mockery.registerMock('../services/spotify_api_service', spotifyMock);
-        mockery.registerMock('../services/host_service', hostMock);
-        mockery.registerMock('request', requestMock);
-
-        app = require('../mock_app');
-        submitRoute = require('../../routes/submit_route');
-        submitRoute.setup(app);
-
-        request(app)
-            .post('/submit')
-            .send({ phoneNumber: 456, existingPlaylistId: 7890 })
-            .set('Cookie', 'access=access_token;refresh=refresh_token;')
-            .expect(302)
-            .end((err, res) => {
-                if (err) {
-                    throw err;
-                }
-
-                expect(access_token).to.be('access_token');
-                expect(refresh_token).to.be('refresh_token');
-
-                expect(route).to.be('generated uri');
-                expect(phoneNumber).to.be(456);
-                expect(playlistId).to.be(7890);
-                expect(username).to.be(1234);
-                expect(isNewPlaylist).to.not.be();
-
-                done();
-            });
-    });
-
-    it('handles a new playlist', (done) => {
-        var access_token,
-            refresh_token,
-            route,
-            phoneNumber,
-            username,
-            playlistId,
-            isNewPlaylist,
-            playlistName,
-            playlistOpts;
-
+    beforeEach(() => {
         const spotifyMock = {
             createPlaylist: (username, newPlaylistName, opts) => {
                 playlistName = newPlaylistName;
@@ -129,7 +62,37 @@ describe('POST /submit', () => {
         app = require('../mock_app');
         submitRoute = require('../../routes/submit_route');
         submitRoute.setup(app);
+    });
 
+    afterEach(() => {
+        mockery.resetCache();
+    });
+
+    it('handles an existing playlist', (done) => {
+        request(app)
+            .post('/submit')
+            .send({ phoneNumber: 456, existingPlaylistId: 7890 })
+            .set('Cookie', 'access=access_token;refresh=refresh_token;')
+            .expect(302)
+            .end((err, res) => {
+                if (err) {
+                    throw err;
+                }
+
+                expect(access_token).to.be('access_token');
+                expect(refresh_token).to.be('refresh_token');
+
+                expect(route).to.be('generated uri');
+                expect(phoneNumber).to.be(456);
+                expect(playlistId).to.be(7890);
+                expect(username).to.be(1234);
+                expect(isNewPlaylist).to.not.be();
+
+                done();
+            });
+    });
+
+    it('handles a new playlist', (done) => {
         request(app)
             .post('/submit')
             .send({ phoneNumber: 456, newPlaylistName: 'new playlist' })
